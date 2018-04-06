4bfe4f0042577b648963c7a74f8381ca06c57337
BenjaminPerrin <perrin.benjamin77@gmail.com>
2018-04-06 04:46:13 +0200

    tour par tour

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbProgressbarConfig } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
+  providers: [NgbProgressbarConfig]
 })
 export class HeroComponent implements OnInit {
 
@@ -14,6 +15,9 @@ export class HeroComponent implements OnInit {
   ids;
   durability;
   powerstats;
+  pvmax;
+  turn = 0;
+  win = 0;
   // durability;
   // idHero;
   // imageHlg;
@@ -41,14 +45,17 @@ export class HeroComponent implements OnInit {
 
   isLive = true;
   hp;
-  constructor(private http: HttpClient) {
-
+  constructor(private http: HttpClient, config: NgbProgressbarConfig) {
+    config.max = 100;
+    config.striped = false;
+    config.animated = true;
+    config.type = 'success';
+    config.height = '20px';
   }
 
   ngOnInit(): void {
     this.ids = [];
     this.selected = [];
-
     this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json').subscribe((data: any) => {
       // this.ids = data;
       for (let index = 0; index < 5; index++) {
@@ -64,23 +71,35 @@ export class HeroComponent implements OnInit {
     this.selected.push(me);
     console.log(this.selected);
   }
+  winGG(n) {
+    if (this.selected[n].powerstats.durability <= 0) {
+      this.win = 1;
+    }
+  }
   attack1() {
+    this.turn = 1;
     this.selected[1].powerstats.durability = this.selected[1].powerstats.durability - 10;
     console.log(this.selected[1].powerstats.durability);
-
+    this.winGG(1);
   }
   attack2() {
+    this.turn = 0;
     this.selected[0].powerstats.durability = this.selected[0].powerstats.durability - 10;
     console.log(this.selected[0].powerstats.durability);
-
+    this.winGG(0);
   }
   attackH1() {
+    this.turn = 1;
     this.selected[1].powerstats.durability = this.selected[1].powerstats.durability - 15;
     console.log(this.selected[1].powerstats.durability);
+    this.winGG(1);
   }
   attackH2() {
+    this.turn = 0;
     this.selected[0].powerstats.durability = this.selected[0].powerstats.durability - 15;
     console.log(this.selected[0].powerstats.durability);
+    this.winGG(0);
   }
+
 }
 
